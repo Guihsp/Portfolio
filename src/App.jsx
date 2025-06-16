d30b5c6f7259f1fb588f22dfdf7ad19e95e73db2
Guilherme Henrique <guilhermehsp26@gmail.com>
2025-06-16 17:35:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/next"
 import { Header } from './components/Global/Header';
 import { AppRoutes } from './services/routes/routes';
 import { ScrollToTop } from './components/Global/ScrollToTop'
@@ -7,6 +8,7 @@ import { ScrollToTop } from './components/Global/ScrollToTop'
 export default function App() {
   return (
     <Router>
+      <Analytics />
       <SpeedInsights />
       <Header />
       <AppRoutes />
